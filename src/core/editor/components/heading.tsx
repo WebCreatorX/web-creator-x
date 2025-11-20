14128a93b2cc479fa5b932b872a4e7d98edae6ae
y-minion <dudals2963@tukorea.ac.kr>
2025-11-20 17:35:17 +0900

    ✨ 헤딩 컴포넌트 생성기 구현 완료

---
+++ src/core/editor/components/heading.tsx
@@ -0,0 +1,22 @@
+import { NodeComponentProps } from "@editor/types/component";
+import { HeadingNode } from "../types/nodes";
+import applyStyles from "../nodeRenerder/applyStyles";
+
+export default function HeadingComponent({
+  node,
+  props,
+  style,
+  children,
+}: NodeComponentProps<HeadingNode>) {
+  const { text, level = "h2" } = props;
+  const inlineStyle = applyStyles(style);
+  const Tag = level;
+
+  return (
+    <div data-component-id={node.id} className={style.className}>
+      <Tag style={inlineStyle}>{text}</Tag>
+      {/* 컨테이너일 경우 이곳에 children이 렌더링 되야 한다. */}
+      {children}
+    </div>
+  );
+}
