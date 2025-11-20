d4326138b4bf9d0be1f024d25b5e4bda51ab111e
y-minion <dudals2963@tukorea.ac.kr>
2025-11-20 16:24:07 +0900

    🏷️ 노드의 스타일 타입 정의

--- src/core/editor/nodeRenerder/applyStyles.ts
+++ src/core/editor/nodeRenerder/applyStyles.ts
@@ -1,14 +1,19 @@
 //참고 자료->https://www.notion.so/Object-assign-2b175c9287fa8077b766de146b87a7e2?source=copy_link
 //추후 성능 개선 필요(메모이제이션)
-import { NodeStyle } from "../types";
+import { CSSProperties } from "react";
+import { NodeStyle } from "../types/styles";
 
 /**
  *
  * @param styleData 노드의 style 데이터: JsonB형태
  *
- * 카테고리별로 중첩되어 있는 스타일 데이터를 평탄화.
+ * 카테고리별로 중첩되어 있는 스타일 데이터를 평탄화하여 인라인 문자열로 반환한다.
+ * 해당 반환값을 바로 style속성의 값으로 삽입할 수 있다.
+ *
  */
-function applyStyles(styleData: NodeStyle) {
+export default function applyStyles(
+  styleData: NodeStyle,
+): CSSProperties | undefined {
   if (!styleData) return;
 
   const combinedStyles = {};
