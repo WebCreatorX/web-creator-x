85a6ac4fb97f29c55b3c04c916e23da3676b047e
y-minion <dudals2963@tukorea.ac.kr>
2025-12-01 15:50:10 +0900

    ✨ 히어로 노드 렌더러 컴포넌트 구현
    전체 레이아웃 사이즈, 위치는 상위 wrapper컴포넌트에게 위임하고, 해당 노드 렌더러 컴포넌트는 컨텐츠 렌더링, 내부 요소 렌더링에 집중하도록 구현

--- packages/ui/src/components/Hero.tsx
+++ packages/ui/src/components/Hero.tsx
@@ -1,7 +1,8 @@
-import applyStyles from "../utils/applyStyles";
 import { NodeComponentProps } from "../types/component";
 import { HeroNode } from "../types/nodes";
 import { useBuilderMode } from "context/builderMode";
+import Image from "next/image";
+import processNodeStyles from "../utils/style";
 
 export default function HeroComponent({
   node,
@@ -9,26 +10,63 @@ export default function HeroComponent({
   style,
 }: NodeComponentProps<HeroNode>) {
   const { mode } = useBuilderMode(); //현재 모드 확인
-  const { heading, subHeading, button, backgroundImage } = props;
-  const inlineStyles = applyStyles(style);
-  const bgImageUrl = backgroundImage ? backgroundImage : null;
 
+  //이 props의 내부 Key에 따라서 하위에 렌더링될 요소들이 결정된다.
+  const { heading, subHeading, button, image } = props;
+
+  //TODO - 🚨 style이나 node가 변경될 때만 재연산되게 useMemo로 메모이제이션 사용해야할 필요가 있다.
+  const nodeStyleObj = processNodeStyles(style);
+
+  /**
+   *
+   * image태그가 z-0에 깔려있고 컨텐츠를 감싸는 컨테이너 div가 image태그 위에 존재합니다.
+   */
   const handleLinkClick = (e: React.MouseEvent) => {
     if (mode === "editor") e.preventDefault();
   };
 
   return (
-    <div>
-      <h1>{heading}</h1>
-      {subHeading && <p>{subHeading}</p>}
-      {button && (
-        <a
-          href={button && button.link}
-          style={inlineStyles}
-          onClick={handleLinkClick}
-          //백그라운드 사진 렌더링 필요
-        ></a>
+    //TODO - 노드들이 드래그 앤 드롭될때 위치가 자유롭게 변하게 할 수 있어야한다.
+    <section
+      data-component-id={node.id}
+      className={`${style.className} flex h-full w-full flex-col items-center justify-center gap-2 overflow-hidden`}
+      style={nodeStyleObj.root}
+    >
+      {/* 배경 이미지 영역 */}
+      {image?.url && (
+        <div className={`absolute inset-0 z-0`} style={nodeStyleObj.image}>
+          <Image
+            fill={true}
+            className={"object-cover"}
+            src={image.url}
+            alt={image.alt || "Hero Background"}
+            priority={true}
+          />
+          <div className={"absolute inset-0 bg-black/40"} />
+        </div>
       )}
-    </div>
+
+      {/* --- 콘텐츠 영역 (z-index를 높여서 이미지 위에 표시) --- */}
+      <div
+        className={"relative top-0 left-0 z-10 flex flex-col items-center p-4"}
+      >
+        <h1
+          style={nodeStyleObj.heading} // heading 스타일 적용 (폰트 크기, 색상)
+        >
+          {heading}
+        </h1>
+        {subHeading && <p style={nodeStyleObj.subHeading}>{subHeading}</p>}
+        {button && (
+          <a
+            href={button.link || "#"}
+            style={nodeStyleObj.button}
+            onClick={handleLinkClick}
+            //백그라운드 사진 렌더링 필요
+          >
+            {button?.text}
+          </a>
+        )}
+      </div>
+    </section>
   );
 }
