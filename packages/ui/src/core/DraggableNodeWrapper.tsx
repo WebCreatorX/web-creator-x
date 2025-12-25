7e9888a9f028dc5bf3e9d4f4b87a1305373f9798
y-minion <dudals2963@tukorea.ac.kr>
2025-12-25 13:25:54 +0900

    ♻️ 에디터 모드의 노드 래퍼 컴포넌트 이원화 작업
    기존에 라이브/에디터 모드에서 모두 사용되면 래퍼 컴포넌트를 에디터 모드에서만 사용되도록 역할 분리 완료. 라이브 모드에서는 LiveModeWrapper래퍼 컴포넌트를 사용하도록 분리 완료.

--- packages/ui/src/core/DraggableNodeWrapper.tsx
+++ packages/ui/src/core/DraggableNodeWrapper.tsx
@@ -1,5 +1,4 @@
-import { useBuilderMode } from "context/builderMode";
-import { CSSProperties } from "react";
+//에디터 모드전용 노드 렌더러 래퍼 컴포넌트
 import { BaseNode } from "types";
 
 interface WrapperProps {
@@ -15,8 +14,7 @@ export default function DraggableNodeWrapper({
   isSelected,
   onDragStart,
 }: WrapperProps) {
-  const { mode } = useBuilderMode();
-  const wrapperStyle: CSSProperties = {
+  const wrapperStyle: React.CSSProperties = {
     position: "absolute", // 핵심: 캔버스 내에서 자유 배치
     left: node["layout"].x,
     top: node["layout"].y,
@@ -25,14 +23,14 @@ export default function DraggableNodeWrapper({
     zIndex: node["layout"].zIndex,
 
     // 선택되었을 때 시각적 피드백 (테두리 등)
-    outline: mode === "editor" && isSelected ? "2px solid blue" : "none",
-    cursor: mode === "editor" ? "move" : "default",
+    outline: isSelected ? "2px solid blue" : "none",
+    cursor: "move",
   };
 
   return (
     <div
       style={wrapperStyle}
-      onMouseDown={(e) => mode === "editor" && onDragStart(e, node.id)}
+      onMouseDown={(e) => onDragStart(e, node.id)}
       className="" //TODO - 필요시 클래스네임 추가(dnd상황에서 스타일 강조 등)
     >
       {/* 실제 컴포넌트(Hero 등)는 이 안에 렌더링됨 */}
@@ -40,7 +38,7 @@ export default function DraggableNodeWrapper({
 
       {/* 리사이즈 핸들 등은 에디터 모드에서만 오버레이로 표시 */}
       {/* TODO-추후 호버시 리사이즈 핸들 렌더링 하도록 수정 필요! */}
-      {isSelected && mode === "editor" && (
+      {isSelected && (
         <>
           <div className="absolute right-0 bottom-0 h-4 w-4 cursor-se-resize bg-blue-500" />
           {/* TODO-기타 리사이즈 핸들들 렌더링은 이곳에서 ... */}
