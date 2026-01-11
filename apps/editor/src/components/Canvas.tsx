91ce7abacbc486c5952d4a525b766a7adf3297e8
y-minion <dudals2963@tukorea.ac.kr>
2026-01-11 16:05:31 +0900

    ♻️ nodes가 비어있을 경우 ROOT를 렌더링 하지 않도록 예외 처리
    Canvas의 nodes가 비어있는 경우 ROOT를 더미 노드가 아닌 실제 노드로 인식해 노드 렌더러로 렌더링 하려고 해 에러 발생. ROOT는 예외처리 하여 노드 렌더러로 렌더링 하지 않도록 처리 완료

--- apps/editor/src/components/Canvas.tsx
+++ apps/editor/src/components/Canvas.tsx
@@ -18,6 +18,8 @@ export default function Canvas() {
   const canvasState = useCanvas();
   const setCanvas = useSetCanvas();
 
+  //FIXME-각 노드들에 key속성 추가해주기. -> 리액트 경고 발생
+  //FIXME-nodes가 비어있는 상황에서 에러발생. -> Base Condition에 Root가 들어간다.(Root는 단지 더미 노드일뿐 로직에 들어가면 안된다.)
   /**
    *
    * @param parentNode 부모 노드 객체
@@ -34,6 +36,7 @@ export default function Canvas() {
     //BaseCondition
     //FIXME-솔직히 !childrenArr만 있어도 될듯? 길이가 0일 수가 없다.
     if (!childrenObjArr || childrenObjArr.length === 0) {
+      if (parentNode.id === null) return;
       return (
         <EditorNodeWrapper
           node={parentNode}
