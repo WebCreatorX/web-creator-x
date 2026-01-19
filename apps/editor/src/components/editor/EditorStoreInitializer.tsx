1c3eb05c9e01bdb77a9a42a7ff4a4a98e11c3d23
y-minion <dudals2963@tukorea.ac.kr>
2026-01-19 11:46:45 +0900

    ♻️ EditorStoreInitializer 리팩토링
    초기에 리렌더링이 될때마다 스토어가 업데이트 되던 현상 수정 완료.
    의존성배열의 값이 참조값이라 리렌더링이 될때마다 계속해서 스토어의 상태도 업데이트되던 문제 발생. useRef를 이용해 마운트 초기 1회만 서버액션에서 받아온 데이터로 업데이트 하고, 이후에는 클라이언트 측에서 데이터를 관리할 예정입니다.

--- apps/editor/src/components/editor/EditorStoreInitializer.tsx
+++ apps/editor/src/components/editor/EditorStoreInitializer.tsx
@@ -1,7 +1,7 @@
 "use client";
 
 import { useSetNode } from "@/stores/useEditorStore";
-import { useEffect } from "react";
+import { useEffect, useRef } from "react";
 import { WcxNode } from "types";
 
 interface EditorStoreInitializer {
@@ -9,14 +9,18 @@ interface EditorStoreInitializer {
   initialNodes: WcxNode[];
 }
 
+//TODO-일단 DB와 연동하지 말자. 서버액션의 사용이유는 초기 렌더링 속도 향상임. 이후에는 클라이언트에서 스토어 업데이트를 할 예정.
 export default function EditorStoreInitializer({
   children,
   initialNodes,
 }: EditorStoreInitializer) {
+  const initialized = useRef(false);
   const setNode = useSetNode();
   useEffect(() => {
+    if (initialized.current) return;
     setNode(initialNodes);
-  }, [initialNodes]);
+    initialized.current = true;
+  }, [initialNodes, setNode]);
 
   return <>{children}</>;
 }
