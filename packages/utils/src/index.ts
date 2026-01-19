8625fe183625eeeea3f2fbdfa8ddf2875a1aff8b
DESKTOP-4N4R3LH\jqk17 <jqk1797@gmail.com>
2026-01-19 09:07:08 +0900

    ✨ cn 유틸리티 함수 구현 (tailwind-merge, clsx)

---
+++ packages/utils/src/index.ts
@@ -0,0 +1,10 @@
+// packages/utils/src/index.ts
+
+// cn.ts에서 정의된 모든 내용을 외부로 내보냅니다.
+export * from './cn';
+
+/**
+ * 나중에 새로운 유틸리티가 추가되면 아래와 같이 계속 추가해나가면 됩니다.
+ * export * from './date';
+ * export * from './storage';
+ */
